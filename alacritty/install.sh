59f34776ed3b54d7073ae3f25aa36957c6d33886
Arnau Llamas <arnau.llamas@gmail.com>
2023-11-11 14:06:01 +0100

    fix colors

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -6,9 +6,8 @@ echo ""
 
 brew install --cask alacritty
 
-# We use Alacritty's default Linux config directory as our storage location here.
-mkdir -p $HOME/.config/alacritty/themes
-git clone https://github.com/alacritty/alacritty-theme $HOME/.config/alacritty/themes
+# Install catppuccin theme
+git clone https://github.com/catppuccin/alacritty.git $HOME/.config/alacritty/catppuccin
 
 echo "Done!"
 echo ""
