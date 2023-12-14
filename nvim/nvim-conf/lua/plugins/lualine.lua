9537be328768561d5f476aebce4273fe2a4fd40a
Arnau Llamas <arnau.llamas@gmail.com>
2023-12-14 22:36:57 +0100

    nicer lualine

--- nvim/nvim-conf/lua/plugins/lualine.lua
+++ nvim/nvim-conf/lua/plugins/lualine.lua
@@ -1,13 +1,12 @@
 return {
-	-- Set lualine as statusline
 	"nvim-lualine/lualine.nvim",
 	-- See `:help lualine.txt`
 	opts = {
 		options = {
-			icons_enabled = false,
+			icons_enabled = true,
 			theme = "catppuccin",
-			component_separators = "|",
-			section_separators = "",
+			component_separators = { left = "|", right = "<" },
+			section_separators = { left = "", right = "" },
 		},
 	},
 }
