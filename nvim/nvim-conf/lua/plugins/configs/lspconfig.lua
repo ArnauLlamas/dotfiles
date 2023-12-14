fd7a83b37eae72b1a1233dac7283c05536a53356
Arnau Llamas <arnau.llamas@gmail.com>
2023-12-14 22:37:49 +0100

    cleaning

--- nvim/nvim-conf/lua/plugins/configs/lspconfig.lua
+++ nvim/nvim-conf/lua/plugins/configs/lspconfig.lua
@@ -1,9 +1,5 @@
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
-	-- NOTE: Remember that lua is a real programming language, and as such it is possible
-	-- to define small helper and utility functions so you don't have to repeat yourself
-	-- many times.
-	--
 	-- In this case, we create a function that lets us more easily define mappings specific
 	-- for LSP related items. It sets the mode, buffer and description for us each time.
 	local nmap = function(keys, func, desc)
@@ -30,13 +26,8 @@ local on_attach = function(_, bufnr)
 
 	-- Lesser used LSP functionality
 	nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-	nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-	nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-	nmap("<leader>wl", function()
-		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, "[W]orkspace [L]ist Folders")
 
-	-- Create a command `:Format` local to the LSP buffer
+	-- Create a command `:LspFormat` local to the LSP buffer
 	vim.api.nvim_buf_create_user_command(bufnr, "LspFormat", function(_)
 		vim.lsp.buf.format()
 	end, { desc = "Format current buffer with LSP" })
@@ -48,13 +39,6 @@ require("mason").setup()
 require("mason-lspconfig").setup()
 
 -- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
---
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property 'filetypes' to the map in question.
 local servers = {
 	pyright = {},
 	docker_compose_language_service = {},
--- nvim/nvim-conf/lua/plugins/configs/treesitter.lua
+++ nvim/nvim-conf/lua/plugins/configs/treesitter.lua
@@ -1,7 +1,6 @@
 -- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
 vim.defer_fn(function()
 	require("nvim-treesitter.configs").setup({
-		-- Add languages to be installed here that you want installed for treesitter
 		ensure_installed = {
 			"bash",
 			"c",
