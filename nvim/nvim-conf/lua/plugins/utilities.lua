09b61c10caa926f8a24200168d2b271392f19799
Arnau Llamas <arnau.llamas@gmail.com>
2023-12-23 22:27:57 +0100

    unify simple utility plugins

---
+++ nvim/nvim-conf/lua/plugins/utilities.lua
@@ -0,0 +1,22 @@
+return {
+	-- Detect tabstop and shiftwidth automatically
+	"tpope/vim-sleuth",
+	{
+		"szw/vim-maximizer",
+		config = function()
+			vim.keymap.set("n", "<leader>mx", "<cmd>MaximizerToggle<CR>", { desc = "[M]a[x]imize current buffer" })
+		end,
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+		config = function()
+			require("markdown-preview").setup()
+			vim.keymap.set("n", "<leader>md", "<cmd>MarkdownPreviewToggle<CR>", { desc = "[M]ark[D]own preview" })
+		end,
+	},
+}
