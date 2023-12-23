6040ca2879f6432c281389e14a267d30e38590fe
Arnau Llamas <arnau.llamas@gmail.com>
2023-12-23 22:26:57 +0100

    unify git plugins

---
+++ nvim/nvim-conf/lua/plugins/git.lua
@@ -0,0 +1,9 @@
+return {
+	"tpope/vim-fugitive",
+	"tpope/vim-rhubarb",
+	-- Adds git related signs to the gutter, as well as utilities for managing changes
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = require("plugins.configs.gitsigns"),
+	},
+}
--- nvim/nvim-conf/lua/plugins/gitsigns.lua
+++
@@ -1,5 +0,0 @@
-return {
-	-- Adds git related signs to the gutter, as well as utilities for managing changes
-	"lewis6991/gitsigns.nvim",
-	opts = require("plugins.configs.gitsigns"),
-}
