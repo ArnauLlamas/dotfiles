b0629b85fe0ad97ecfa3e920ee7483f6b79f0b92
Arnau Llamas <arnau.llamas@gmail.com>
2024-01-13 14:25:30 +0100

    move lspconfing and none-ls to same file

---
+++ nvim/nvim-conf/lua/plugins/lsp.lua
@@ -0,0 +1,44 @@
+return {
+	{
+		-- LSP Configuration & Plugins
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- Automatically install LSPs to stdpath for neovim
+			{
+				"williamboman/mason.nvim",
+				config = function()
+					return require("plugins.configs.mason")
+				end,
+			},
+			"williamboman/mason-lspconfig.nvim",
+
+			-- Useful status updates for LSP
+			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+			{
+				"j-hui/fidget.nvim",
+				config = function()
+					require("fidget").setup({
+						notification = {
+							window = {
+								winblend = 0,
+							},
+						},
+					})
+				end,
+			},
+
+			-- Additional lua configuration, makes nvim stuff amazing!
+			{ "folke/neodev.nvim", opts = {} },
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+		end,
+	},
+	{
+		"nvimtools/none-ls.nvim",
+		-- ft = { "python", "go", "terraform", "hcl", "yaml", "lua" },
+		opts = function()
+			return require("plugins.configs.none-ls")
+		end,
+	},
+}
--- nvim/nvim-conf/lua/plugins/none-ls.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"nvimtools/none-ls.nvim",
-	-- ft = { "python", "go", "terraform", "hcl", "yaml", "lua" },
-	opts = function()
-		return require("plugins.configs.none-ls")
-	end,
-}
