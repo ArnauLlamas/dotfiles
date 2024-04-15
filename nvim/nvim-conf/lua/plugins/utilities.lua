a61eb2372bdcdad48c08181dd72dda287d5fcd68
Arnau Llamas <arnau.llamas@gmail.com>
2024-04-15 17:22:32 +0200

    reduce time notifications are on screen

--- nvim/nvim-conf/lua/plugins/utilities.lua
+++ nvim/nvim-conf/lua/plugins/utilities.lua
@@ -4,50 +4,57 @@ return {
 	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
-		opts = {
-			routes = {
-				{
-					filter = {
-						event = "msg_show",
-						kind = "",
-						find = "written",
-					},
-					opts = { skip = true },
-				},
-			},
-			views = {
-				cmdline_popup = {
-					border = {
-						style = "none",
-						padding = { 1, 3 },
-					},
-					filter_options = {},
-					win_options = {
-						winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-					},
-				},
-			},
-			lsp = {
-				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-				},
-			},
-			presets = {
-				bottom_search = true, -- use a classic bottom cmdline for search
-				command_palette = false, -- position the cmdline and popupmenu together
-				long_message_to_split = true, -- long messages will be sent to a split
-				inc_rename = false, -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = false, -- add a border to hover docs and signature help
-			},
-		},
 		dependencies = {
 			"MunifTanjim/nui.nvim",
 			"hrsh7th/nvim-cmp",
 			"rcarriga/nvim-notify",
 		},
+		config = function()
+			require("notify").setup({
+				timeout = 1500,
+				stages = "static",
+			})
+
+			require("noice").setup({
+				routes = {
+					{
+						filter = {
+							event = "msg_show",
+							kind = "",
+							find = "written",
+						},
+						opts = { skip = true },
+					},
+				},
+				views = {
+					cmdline_popup = {
+						border = {
+							style = "none",
+							padding = { 1, 3 },
+						},
+						filter_options = {},
+						win_options = {
+							winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+						},
+					},
+				},
+				lsp = {
+					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+					},
+				},
+				presets = {
+					bottom_search = true,    -- use a classic bottom cmdline for search
+					command_palette = false, -- position the cmdline and popupmenu together
+					long_message_to_split = true, -- long messages will be sent to a split
+					inc_rename = false,      -- enables an input dialog for inc-rename.nvim
+					lsp_doc_border = false,  -- add a border to hover docs and signature help
+				},
+			})
+		end,
 	},
 	-- Detect tabstop and shiftwidth automatically
 	"tpope/vim-sleuth",
@@ -79,11 +86,13 @@ return {
 			require("gen").setup(opts)
 
 			require("gen").prompts["DevOps Me!"] = {
-				prompt = "You are a senior devops engineer, acting as an assistant. You offer help with cloud technologies like: Terraform, terragrunt, AWS, kubernetes. You answer with code examples when possible. $input:\n$text",
+				prompt =
+				"You are a senior devops engineer, acting as an assistant. You offer help with cloud technologies like: Terraform, terragrunt, AWS, kubernetes. You answer with code examples when possible. $input:\n$text",
 				replace = true,
 			}
 			require("gen").prompts["Pair Programmer"] = {
-				prompt = "You are a senior software developer, acting as an assistant. You offer help with software web technologies and frameworks like: javascript, typescript, astro, go, python, lua. You answer with code examples when possible. $input:\n$text",
+				prompt =
+				"You are a senior software developer, acting as an assistant. You offer help with software web technologies and frameworks like: javascript, typescript, astro, go, python, lua. You answer with code examples when possible. $input:\n$text",
 				replace = true,
 			}
 
