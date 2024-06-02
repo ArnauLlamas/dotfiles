1b218d85af15902b353972e64c30e165954120c8
Arnau Llamas <arnau.llamas@gmail.com>
2024-06-02 14:17:39 +0200

    move noice.nvim with theme plugins

--- nvim/nvim-conf/lua/plugins/theme.lua
+++ nvim/nvim-conf/lua/plugins/theme.lua
@@ -32,6 +32,61 @@ return {
 		},
 	},
 	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"hrsh7th/nvim-cmp",
+			"rcarriga/nvim-notify",
+		},
+		config = function()
+			require("notify").setup({
+				timeout = 1500,
+				stages = "static",
+			})
+
+			require("noice").setup({
+				routes = {
+					{
+						filter = {
+							event = "msg_show",
+							kind = "",
+							find = "written",
+						},
+						opts = { skip = true },
+					},
+				},
+				views = {
+					cmdline_popup = {
+						border = {
+							style = "none",
+							padding = { 1, 3 },
+						},
+						filter_options = {},
+						win_options = {
+							winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+						},
+					},
+				},
+				lsp = {
+					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+					},
+				},
+				presets = {
+					bottom_search = true, -- use a classic bottom cmdline for search
+					command_palette = false, -- position the cmdline and popupmenu together
+					long_message_to_split = true, -- long messages will be sent to a split
+					inc_rename = false, -- enables an input dialog for inc-rename.nvim
+					lsp_doc_border = false, -- add a border to hover docs and signature help
+				},
+			})
+		end,
+	},
+	{
 		"nvimdev/dashboard-nvim",
 		event = "VimEnter",
 		config = function()
