c0de09a106c9adf913a43473a9cda3348c7833ec
Arnau Llamas <arnau.llamas@gmail.com>
2024-06-02 14:42:36 +0200

    upgrade plugins

--- nvim/nvim-conf/lua/plugins/debug.lua
+++ nvim/nvim-conf/lua/plugins/debug.lua
@@ -1,13 +1,53 @@
 return {
 	{
 		"folke/trouble.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("trouble").setup()
-			vim.keymap.set("n", "<leader>q", "<cmd>TroubleToggle<CR>", { desc = "Open trouble list" })
-		end,
-	},
-	-- {
+		opts = {
+			modes = {
+				test = {
+					mode = "diagnostics",
+					preview = {
+						type = "split",
+						relative = "win",
+						position = "right",
+						size = 0.3,
+					},
+				},
+			},
+		}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	}, -- {
 	-- 	"mfussenegger/nvim-dap",
 	-- 	dependencies = {
 	-- 		-- Creates a beautiful debugger UI
