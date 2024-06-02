d02a851782994c24ebc350290461fa812a3958ad
Arnau Llamas <arnau.llamas@gmail.com>
2024-06-02 14:16:39 +0200

    change some remaps

--- nvim/nvim-conf/lua/plugins/comment.lua
+++ nvim/nvim-conf/lua/plugins/comment.lua
@@ -2,21 +2,23 @@ return {
 	-- "gc" to comment visual regions/lines
 	"numToStr/Comment.nvim",
 	opts = {},
+	lazy = false,
 	-- If any day this PR is merged delete this custom config block
 	-- https://github.com/numToStr/Comment.nvim/pull/398
 	config = function()
 		local ft = require("Comment.ft")
 		ft.hcl = { "#%s", "/*%s*/" }
-		ft.helm = { "{{- /*%s*/}}" }
-		-- Toggle comment
-		vim.keymap.set("n", "<leader>/", function()
-			require("Comment.api").toggle.linewise.current()
-		end, { desc = "Toggle comment" })
-		vim.keymap.set(
-			"v",
-			"<leader>/",
-			"<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
-			{ desc = "Toggle comment" }
-		)
+		ft.helm = { "{{- /*%s*/ }}" }
+		require("Comment").setup()
+		-- 	-- Toggle comment
+		-- 	vim.keymap.set("n", "<leader>/", function()
+		-- 		require("Comment.api").toggle.linewise.current()
+		-- 	end, { desc = "Toggle comment" })
+		-- 	vim.keymap.set(
+		-- 		"v",
+		-- 		"<leader>/",
+		-- 		"<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+		-- 		{ desc = "Toggle comment" }
+		-- )
 	end,
 }
