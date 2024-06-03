a2cbe2c6d1977152771269d56ae549efde9bd783
Arnau Llamas <arnau.llamas@gmail.com>
2024-06-03 09:53:21 +0200

    migrate from omz + starship to zinit + p10k

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -1,60 +1,106 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+# Helper function
 source_if_exists () {
   if test -r "$1"; then
     source "$1"
   fi
 }
 
-autoload -Uz compinit && compinit
-autoload -Uz compdef
+# Custom variables
+export TERM="xterm-256color"
+export EDITOR="nvim"
 
+export REPOS=$HOME/Documents/repos
+export PERSONAL=$HOME/Documents/personal
+
+export GOPATH="${HOME}/go"
+
+export PATH=$PATH:$HOME/bin:/opt/homebrew/bin:/usr/local/go/bin:$HOME/.local/share/nvim/mason/bin:$HOME/.local/bin:$HOME/.fzf/bin:$GOPATH/bin
+
+# Custom sourcing
 source_if_exists $HOME/.env.sh
+source_if_exists $DOTFILES/zsh/aliases.zsh
 
-# oh-my-zsh
-export ZSH="$HOME/.oh-my-zsh"
-ZSH_THEME=""
+# Creds sourcing
+for file in $(ls $HOME/.creds)
+do
+  source_if_exists "${JOME}/.creds/${file}"
+done
 
-# ASDF Sourcing
+## System specifics sourcing
 if [[ "$(uname)" == "Linux" ]]; then
   source $HOME/.asdf/asdf.sh
 elif [[ "$(uname)" == "Darwin" ]]; then
   source /opt/homebrew/opt/asdf/libexec/asdf.sh
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
-# ohmyzsh plugins
-plugins=(gitfast aws asdf terraform brew kind golang)
 
-source $ZSH/oh-my-zsh.sh
+# Set the directory we want to store zinit and plugins
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/.share}/zinit/zinit.git"
 
-export PATH=$PATH:$HOME/bin:/opt/homebrew/bin:/usr/local/go/bin:$HOME/.local/share/nvim/mason/bin:$HOME/.local/bin:$HOME/.fzf/bin
-export GOROOT="$(go env GOROOT)"
-export GOPATH="$(go env GOPATH)"
-export TERM="xterm-256color"
+# Download Zinit, if it's not there yet
+if [ ! -d "$ZINIT_HOME" ]; then
+  mkdir -p "$(dirname $ZINIT_HOME)"
+  git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+fi
 
-# Creds sourcing
-$(for file in $HOME/.creds/*
-do
-  source_if_exists $file
-done) || echo "no creds loaded..."
+# Source/Load zinit
+source "${ZINIT_HOME}/zinit.zsh"
 
-# Repos folder
-export REPOS=$HOME/Documents/repos
-export PERSONAL=$HOME/Documents/personal
+# Add in Powerlevel10k
+zinit ice depth=1; zinit light romkatv/powerlevel10k
 
-#Star Ship
-eval "$(starship init zsh)"
+# Add in zsh plugins
+zinit light zsh-users/zsh-syntax-highlighting
+zinit light zsh-users/zsh-completions
+zinit light zsh-users/zsh-autosuggestions
 
-# Placing it higher makes it partially overwritten (ls...) for unknown reasons :/
-source_if_exists $DOTFILES/zsh/aliases.zsh
+# Add in snippets
+zinit snippet OMZP::aws
+zinit snippet OMZP::asdf
+zinit snippet OMZP::terraform
+zinit snippet OMZP::brew
+zinit snippet OMZP::golang
+zinit snippet OMZP::kubectl
+zinit snippet OMZP::kubectx
+zinit snippet OMZP::command-not-found
 
-# Set up fzf key bindings
-source <(fzf --zsh)
+# Load completions
+autoload -U compinit && compinit
+
+zinit cdreplay -q
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+# Keybindings
+bindkey -e
 
 # History
 HISTFILE="$HOME"/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
+HISTDUP=erase
 setopt appendhistory
-setopt histignorespace
+setopt sharehistory
+setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+setopt hist_ignore_dups
+setopt hist_find_no_dups
 
-export EDITOR="nvim"
+# Completion styling
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*' menu select
+zmodload zsh/complist
 
+# Shell integrations
+source <(fzf --zsh)
 eval "$(zoxide init --cmd cd zsh)"
