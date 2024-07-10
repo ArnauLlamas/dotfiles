f34f7566a7ba50204cffe22129bd869c070fb8b6
Arnau Llamas <arnau.llamas@gmail.com>
2024-07-10 20:55:53 +0200

    add special-dirs autocompletions

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -146,6 +146,7 @@ setopt hist_ignore_dups
 setopt hist_find_no_dups
 
 # Completion styling
+zstyle ':completion:*' special-dirs true 
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 zstyle ':completion:*' menu select
