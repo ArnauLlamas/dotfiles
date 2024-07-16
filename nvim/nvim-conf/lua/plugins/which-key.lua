e0c24fe813f553c1cd49c2d7917d3e2716ce8a9f
Arnau Llamas <arnau.llamas@gmail.com>
2024-07-16 21:45:20 +0200

    chore(which-key): migrate to v3 conf

--- nvim/nvim-conf/lua/plugins/which-key.lua
+++ nvim/nvim-conf/lua/plugins/which-key.lua
@@ -4,23 +4,23 @@ return {
 	opts = {},
 	config = function()
 		-- document existing key chains
-		require("which-key").register({
-			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-			["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-			["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
-			["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
-			["<leader>f"] = { name = "[F]ind", _ = "which_key_ignore" },
-			["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-			["<leader>l"] = { name = "[L]SP", _ = "which_key_ignore" },
-			["<leader>s"] = { name = "[S]plit", _ = "which_key_ignore" },
-			["<leader>x"] = { name = "Trouble", _ = "which_key_ignore" },
+		require("which-key").add({
+			{ "<leader>c", group = "[C]ode" },
+			{ "<leader>d", group = "[D]ocument" },
+			{ "<leader>g", group = "[G]it" },
+			{ "<leader>h", group = "Git [H]unk" },
+			{ "<leader>f", group = "[F]ind" },
+			{ "<leader>t", group = "[T]oggle" },
+			{ "<leader>w", group = "[W]orkspace" },
+			{ "<leader>l", group = "[L]SP" },
+			{ "<leader>s", group = "[S]plit" },
+			{ "<leader>x", group = "Trouble" },
 		})
 		-- register which-key VISUAL mode
 		-- required for visual <leader>hs (hunk stage) to work
-		require("which-key").register({
-			["<leader>"] = { name = "VISUAL <leader>" },
-			["<leader>h"] = { "Git [H]unk" },
-		}, { mode = "v" })
+		require("which-key").add({
+			{ "<leader>", group = "VISUAL <leader>", mode = "v" },
+			{ "<leader>h", desc = "Git [H]unk", mode = "v" },
+		})
 	end,
 }
