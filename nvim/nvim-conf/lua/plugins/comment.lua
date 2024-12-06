77720d639034de581d3be02649b6eeb1552b9868
Arnau Llamas <arnau.llamas@gmail.com>
2024-12-06 17:43:12 +0100

    comment.nvim supports hcl files natively now

--- nvim/nvim-conf/lua/plugins/comment.lua
+++ nvim/nvim-conf/lua/plugins/comment.lua
@@ -3,22 +3,9 @@ return {
 	"numToStr/Comment.nvim",
 	opts = {},
 	lazy = false,
-	-- If any day this PR is merged delete this custom config block
-	-- https://github.com/numToStr/Comment.nvim/pull/398
 	config = function()
 		local ft = require("Comment.ft")
-		ft.hcl = { "#%s", "/*%s*/" }
 		ft.helm = { "{{- /*%s*/ }}" }
 		require("Comment").setup()
-		-- 	-- Toggle comment
-		-- 	vim.keymap.set("n", "<leader>/", function()
-		-- 		require("Comment.api").toggle.linewise.current()
-		-- 	end, { desc = "Toggle comment" })
-		-- 	vim.keymap.set(
-		-- 		"v",
-		-- 		"<leader>/",
-		-- 		"<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
-		-- 		{ desc = "Toggle comment" }
-		-- )
 	end,
 }
