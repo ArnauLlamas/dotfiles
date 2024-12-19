15fd7e5260bf48580b510e438919c603cb719897
Arnau Llamas <arnau.llamas@gmail.com>
2024-12-19 22:24:16 +0100

    add copilot and autopairs

--- nvim/nvim-conf/lua/plugins/autocompletion.lua
+++ nvim/nvim-conf/lua/plugins/autocompletion.lua
@@ -1,12 +1,33 @@
 return {
 	{
 		"saghen/blink.cmp",
-		dependencies = "rafamadriz/friendly-snippets",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			"giuxtaposition/blink-cmp-copilot",
+		},
 
 		version = "v0.*",
 
 		opts = {
-			keymap = { preset = "default" },
+			keymap = {
+				preset = "default",
+				-- keymap = { ["<CR>"] = { "select_and_accetp", "fallback" } },
+			},
+
+			sources = {
+				providers = {
+					copilot = {
+						name = "copilot",
+						module = "blink-cmp-copilot",
+						score_offset = 100,
+						async = true,
+					},
+				},
+			},
+
+			completion = {
+				enabled_sources = { "lsp", "path", "snippets", "buffer", "copilot" },
+			},
 
 			appearance = {
 				use_nvim_cmp_as_default = true,
@@ -14,6 +35,8 @@ return {
 			},
 
 			signature = { enabled = true },
+			-- accept = { auto_brackets = { enabled = true } },
+			documentation = { auto_show = true },
 		},
 	},
 }
